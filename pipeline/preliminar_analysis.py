6cea847826af6e93e06f4adca7dd4c1f58b45939
Emilio Garcia <egarcia@ebi.ac.uk>
2021-10-30 16:03:04 +0100

    Join sets

--- pipeline/preliminar_analysis.py
+++ pipeline/preliminar_analysis.py
@@ -14,33 +14,22 @@ input_validated = glob.glob(base_dir + '/input/validated/*.bed')
 input_previous = glob.glob(base_dir + '/input/previous/*_chr.bed')
 
 
-# This function expects a file name in the form: strand_caller.vcf (i.e: /some/path/dba2j_pbsv.vcf)
-def file_details_2(file_path):
-    file_name = file_path.split("/")[-1]
-    split_name = file_name.split("_")
-    first = split_name[0]
-    second = split_name[1].replace(".vcf", "").replace(".bed", "")
-    return file_name, first, second
+# This function always returns an array with: base_dir, extension, part1, part2, part3 ..., part_n
+def path_info(file_path, splitter="_"):
+    split = os.path.split(file_path)
+    splitext = os.path.splitext(split[1])
+    name_parts = splitext[0].split(splitter)
+    return [split[0], splitext[1]] + name_parts
 
 
-# This function expects a file name in the form: strand_type_subtype.bed (i.e: /some/path/dba2j_ins_h6.bed)
-def file_details_3(file_path):
-    file_name = file_path.split("/")[-1]
-    split_name = file_name.split("_")
-    first = split_name[0]
-    second = split_name[1]
-    third = split_name[2].replace(".bed", "")
-    return file_name, first, second, third
-
-
+# Generate TSV files with human readable info from VCF
 def prepare_exploratory_data(vcf_file_path):
-    # TODO: Can infer the command using a list of columns to be included in the query
     query_sniffles = """ 
         echo '#CALLER\\tIDX\\tCHROM\\tPOS\\tEND\\tSVLEN\\tSVTYPE\\tFILTER\\tRE\\tAF\\tGT\\tDR\\tDV\\tSVLEN_ABS\\tAF_BIN\\tSVLEN_BIN' > {output} && 
         {bcf_bin} query -Hf'%CHROM\\t%POS0\\t%END0\\t[%SVLEN]\\t[%SVTYPE]\\t%FILTER\\t[%RE]\\t[%AF]\\t[%GT]\\t[%DR]\\t[%DV]\\n' {vcf_file} | 
         awk -F'\\t' 'BEGIN {{OFS = FS}} $1 ~/^[1-9]*$|^X$/{{ 
             abs=$4<0?-$4:$4; af=(5-(int($7*100)%5)+int($7*100))/100; idx=$1":"$2"-"$3; 
-            len_bin=abs<30?"0-30":abs<50?"30-50":abs<100?"50-100":abs<500?"100-500":"500+";  
+            len_bin=abs<30?"0-30":abs<50?"30-50":abs<100?"50-100":abs<500?"100-500":abs<2000?"500-2000":abs<10000?"2000-10000":abs<50000?"10000-50000":"large";  
             print "{caller}",idx,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,abs,af,len_bin
         }}' >> {output}
     """
@@ -50,12 +39,14 @@ def prepare_exploratory_data(vcf_file_path):
         {bcf_bin} query -Hf'%CHROM\\t%POS0\\t%END0\\t[%SVLEN]\\t[%SVTYPE]\\t%FILTER\\t[%GT]\\t[%AD]\\t[%DP]\\n' {vcf_file} | 
         awk -F'\\t' 'BEGIN {{OFS = FS}} $1 ~/^[1-9]*$|^X$/{{
             abs=$4<0?-$4:$4; af=(5-(int($7*100)%5)+int($7*100))/100; idx=$1":"$2"-"$3;
-            len_bin=abs<30?"0-30":abs<50?"30-50":abs<100?"50-100":abs<500?"100-500":"500+";
+            len_bin=abs<30?"0-30":abs<50?"30-50":abs<100?"50-100":abs<500?"100-500":abs<2000?"500-2000":abs<10000?"2000-10000":abs<50000?"10000-50000":"large";
             print "{caller}",idx,$1,$2,$3,$4,$5,$6,$7,$8,$9,abs,len_bin
         }}' >> {output}
     """
 
-    file_name, strain, caller = file_details_2(vcf_file_path)
+    info = path_info(vcf_file_path)
+    strain = info[2]
+    caller = info[3]
     print("Preparing stats file for [strain: " + strain + " - caller: " + caller + "]")
 
     output_file = base_dir + "/work/explore/raw/" + strain + "_" + caller + ".tsv"
@@ -72,13 +63,16 @@ def prepare_exploratory_data(vcf_file_path):
 # This function looks for a exploratory file and generates a bed4 from it
 def bed_and_intersect_from_exploratory_file(vcf_file_path, window):
     bed_pbsv = """
-        awk -F'\\t' 'BEGIN {{OFS = FS}} {{ print $3==\"CHROM\"?$3:\"chr\"$3,$4-{window},$5+{window},$12 }}' {input} > {output}
+        awk -F'\\t' 'BEGIN {{OFS = FS}} {{ print $3==\"CHROM\"?"#"$3:\"chr\"$3,$4-{window},$5+{window},$12 }}' {input} > {output}
     """
     bed_sniffles = """
-        awk -F'\\t' 'BEGIN {{OFS = FS}} {{ print $3==\"CHROM\"?$3:\"chr\"$3,$4-{window},$5+{window},$14 }}' {input} > {output}
+        awk -F'\\t' 'BEGIN {{OFS = FS}} {{ print $3==\"CHROM\"?"#"$3:\"chr\"$3,$4-{window},$5+{window},$14 }}' {input} > {output}
     """
 
-    file_name, strain, caller = file_details_2(vcf_file_path)
+    info = path_info(vcf_file_path)
+    strain = info[2]
+    caller = info[3]
+
     exploratory_expected_path = base_dir + "/work/explore/raw/" + strain + "_" + caller + ".tsv"
     generated_bed = base_dir + "/work/explore/bed/" + strain + "_" + caller + ".bed"
 
@@ -106,10 +100,12 @@ def bed_and_intersect_from_exploratory_file(vcf_file_path, window):
 def intersect_with(bed_in, files_with, intersect_out, window=0, caller=''):
     # Clear output file and add header
     with open(intersect_out, 'w') as out_file:
-        out_file.write('#CALLER\tIDX\tCHROM\tPOS\tEND\tSVLEN\tTYPE\tCHROM\tPOS\tEND\tSVLEN\tTYPE\tOVERLAP\n')
+        out_file.write(
+            '#CALLER\tIDX\tCHROM_A\tPOS_A\tEND_A\tSVLEN_A\tTYPE_A\tCHROM_B\tPOS_B\tEND_B\tSVLEN_B\tTYPE_B\tOVERLAP\n')
 
     for file in files_with:
-        file_name, strain, sv_type = file_details_2(file)
+        info = path_info(file)
+        sv_type = info[3]
         intersect_beds(file, bed_in, intersect_out, window, sv_type, caller=caller)
 
 
@@ -130,17 +126,39 @@ def intersect_beds(bed_a, bed_b, output, window=0, a_type='', b_type='', caller=
     print(stream.read())
 
 
+# Combine TSV files into a single XLSX file
 def merge_tsv_files():
     print("Creating merged Excel file")
     writer = pd.ExcelWriter(base_dir + '/work/explore/merged.xlsx')  # Arbitrary output name
     for tsv_file in glob.glob(base_dir + '/work/explore/raw/*.tsv'):
-        sheet_name = tsv_file.replace(tsv_file.replace(".tsv", "").split("_")[0] + "_", "").replace(".tsv", "")
+        sheet_name = "_".join(path_info(tsv_file)[2:])
         print("Processing sheet: " + sheet_name)
         df = pd.read_csv(tsv_file, sep='\t', low_memory=False)
         df.to_excel(writer, sheet_name=sheet_name, index=False)
     writer.save()
 
 
+# Join validated intersections with original new calls
+def join_with_intersect(set_identifier):
+    for tsv_file in glob.glob(base_dir + '/work/explore/raw/*_intersect_' + set_identifier + '.tsv'):
+        info = path_info(tsv_file)
+        folder = info[0]
+        strain = info[2]
+        caller = info[3]
+
+        original_calls = folder + "/" + strain + "_" + caller + ".tsv"
+        join_output = folder + "/" + strain + "_" + caller + "_join_" + set_identifier + ".tsv"
+
+        print("Joining new reads with " + set_identifier + " intersect: " + tsv_file + " <--> " + original_calls)
+
+        intersect_df = pd.read_csv(tsv_file, sep='\t', low_memory=False)
+        calls_df = pd.read_csv(original_calls, sep='\t', low_memory=False)
+
+        join_df = pd.merge(intersect_df, calls_df, on="IDX")
+        join_df.to_csv(join_output, sep='\t', index=False)
+
+
+# Initialize directories
 def initialize(dir_path):
     paths = ['/input/calls', '/input/validated', '/input/previous',
              '/work/explore/raw', '/work/explore/bed',
@@ -155,4 +173,6 @@ for vcf_file in input_calls:
     prepare_exploratory_data(vcf_file)
     bed_and_intersect_from_exploratory_file(vcf_file_path=vcf_file, window=50)
 
-merge_tsv_files()
+# merge_tsv_files()
+join_with_intersect("validated")
+join_with_intersect("previous")
