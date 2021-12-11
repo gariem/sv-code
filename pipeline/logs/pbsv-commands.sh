b5ec263e889ccfd57614a361c33253dc7f0121bf
Emilio Garcia <egarcia@ebi.ac.uk>
2021-12-11 13:22:52 +0000

    PBSV Discover

--- pipeline/logs/pbsv-commands.sh
+++ pipeline/logs/pbsv-commands.sh
@@ -24,8 +24,8 @@ bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY
 
 
 # Initial variables
-THREADS=12
-BMEMORY=160000
+THREADS=8
+BMEMORY=120000
 
 HOME=/hps/nobackup/production/mousegenomes/emilio/final
 STRAIN=JF1_MsJ
@@ -50,7 +50,7 @@ bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY
 
 # Initial variables
 THREADS=8
-BMEMORY=160000
+BMEMORY=120000
 
 HOME=/hps/nobackup/production/mousegenomes/emilio/final
 STRAIN=129S1_SvImJ
@@ -63,6 +63,112 @@ JOB_DIR=$HOME/pbsv_out
 
 JOB_ERR=$JOB_DIR/$STRAIN-disc.err
 JOB_OUT=$JOB_DIR/$STRAIN-disc.out
+
+OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
+
+COMMAND_DISCOVER="pbsv discover $ALIGNED_READS $OUTPUT_SVSIG"
+
+# Discoversubmission
+bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY] span[hosts=1]" -M$BMEMORY -o $JOB_OUT -e $JOB_ERR -n $THREADS "$COMMAND_DISCOVER"
+
+
+
+# Initial variables
+THREADS=8
+BMEMORY=120000
+HOME=/hps/nobackup/production/mousegenomes/emilio/final
+REFERENCE=$HOME/data/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+
+STRAIN=A_J
+ALIGNED_READS=$HOME/minimap2/$STRAIN.sorted.bam
+
+JOB_ERR=$HOME/pbsv_out/$STRAIN-disc.err
+JOB_OUT=$HOME/pbsv_out/$STRAIN-disc.out
+
+OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
+
+COMMAND_DISCOVER="pbsv discover $ALIGNED_READS $OUTPUT_SVSIG"
+
+# Discoversubmission
+bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY] span[hosts=1]" -M$BMEMORY -o $JOB_OUT -e $JOB_ERR -n $THREADS "$COMMAND_DISCOVER"
+
+
+
+# Initial variables
+THREADS=8
+BMEMORY=180000
+HOME=/hps/nobackup/production/mousegenomes/emilio/final
+REFERENCE=$HOME/data/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+
+STRAIN=AKR_J
+ALIGNED_READS=$HOME/minimap2/$STRAIN.sorted.bam
+
+JOB_ERR=$HOME/pbsv_out/$STRAIN-disc.err
+JOB_OUT=$HOME/pbsv_out/$STRAIN-disc.out
+
+OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
+
+COMMAND_DISCOVER="pbsv discover $ALIGNED_READS $OUTPUT_SVSIG"
+
+# Discoversubmission
+bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY] span[hosts=1]" -M$BMEMORY -o $JOB_OUT -e $JOB_ERR -n $THREADS "$COMMAND_DISCOVER"
+
+
+
+# Initial variables
+THREADS=8
+BMEMORY=120000
+HOME=/hps/nobackup/production/mousegenomes/emilio/final
+REFERENCE=$HOME/data/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+
+STRAIN=BALB_cJ
+ALIGNED_READS=$HOME/minimap2/$STRAIN.sorted.bam
+
+JOB_ERR=$HOME/pbsv_out/$STRAIN-disc.err
+JOB_OUT=$HOME/pbsv_out/$STRAIN-disc.out
+
+OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
+
+COMMAND_DISCOVER="pbsv discover $ALIGNED_READS $OUTPUT_SVSIG"
+
+# Discoversubmission
+bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY] span[hosts=1]" -M$BMEMORY -o $JOB_OUT -e $JOB_ERR -n $THREADS "$COMMAND_DISCOVER"
+
+
+
+# Initial variables
+THREADS=8
+BMEMORY=180000
+HOME=/hps/nobackup/production/mousegenomes/emilio/final
+REFERENCE=$HOME/data/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+
+STRAIN=C3H_HeJ
+ALIGNED_READS=$HOME/minimap2/$STRAIN.sorted.bam
+
+JOB_ERR=$HOME/pbsv_out/$STRAIN-disc.err
+JOB_OUT=$HOME/pbsv_out/$STRAIN-disc.out
+
+OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
+
+COMMAND_DISCOVER="pbsv discover $ALIGNED_READS $OUTPUT_SVSIG"
+
+# Discoversubmission
+bsub -J "$STRAIN" -R "select[type==X86_64 && mem > $BMEMORY] rusage[mem=$BMEMORY] span[hosts=1]" -M$BMEMORY -o $JOB_OUT -e $JOB_ERR -n $THREADS "$COMMAND_DISCOVER"
+
+
+
+
+# Initial variables
+THREADS=8
+BMEMORY=180000
+HOME=/hps/nobackup/production/mousegenomes/emilio/final
+REFERENCE=$HOME/data/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+
+STRAIN=CAST_EiJ
+ALIGNED_READS=$HOME/minimap2/$STRAIN.sorted.bam
+
+JOB_ERR=$HOME/pbsv_out/$STRAIN-disc.err
+JOB_OUT=$HOME/pbsv_out/$STRAIN-disc.out
 
 OUTPUT_SVSIG=$HOME/pbsv/$STRAIN.svsig.gz
 
